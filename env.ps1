1f9e6fb2da147b4c8e95447ca7ec52d87f3422b7
chen bojian <chenbojian@foxmail.com>
2016-01-12 23:36:40 +0800

    Update env.ps1

--- env.ps1
+++ env.ps1
@@ -1,3 +1,13 @@
-Function vi {
-    st $args
+function Compare-Version($ver1, $ver2) {
+    $v1 = [System.Version]$ver1
+    $v2 = [System.Version]$ver2
+    if ($v1 -gt $v2) return 1
+    if ($v1 -eq $v2) return 0
+    return -1
+}
+
+if (Test-Path "${env:ProgramFiles(x86)}\vim\vim74\gvim.exe") {
+    Set-Alias -scope global vi "${env:ProgramFiles(x86)}\vim\vim74\gvim.exe"
+} else {
+    function global:vi { st $args }
 }

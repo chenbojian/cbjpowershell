aa674edaf43aa3ec1d348ceec892c77c5ef4f4a9
chen bojian <chenbojian@foxmail.com>
2016-01-13 18:03:20 +0800

    Update env.ps1

--- env.ps1
+++ env.ps1
@@ -1,8 +1,8 @@
 function Compare-Version($ver1, $ver2) {
     $v1 = [System.Version]$ver1
     $v2 = [System.Version]$ver2
-    if ($v1 -gt $v2) return 1
-    if ($v1 -eq $v2) return 0
+    if ($v1 -gt $v2) {return 1}
+    if ($v1 -eq $v2) {return 0}
     return -1
 }
 
